1390498cbc49df7246d513190eba11b6101bd7e3
sennasemakula <sennasemakula@hotmail.co.uk>
2018-10-06 18:46:05 +0100

    Removed whitespaces

--- app/plot.py
+++ app/plot.py
@@ -1,7 +1,6 @@
 from graph import Graph
 from bokeh.plotting import figure, output_file, show
 
-
 """Class that will generate a simple plot chart"""
 class Plot(Graph):
 	def __init__(self, data, title):
@@ -11,7 +10,6 @@ class Plot(Graph):
 	def draw_graph(self, x_axis, y_axis):
 		graph = figure(title=self.title, x_axis_label='x', y_axis_label='y')
 		graph.line(x_axis, y_axis, legend="Temp.", line_width=2)
-
 		show(graph)
 
 		return self.generate_file("Movies")
