e686de31d35eb5a128fac7ffe9401dbd8a106fb3
sennasemakula <sennasemakula@hotmail.co.uk>
2018-10-06 19:58:15 +0100

    Fixed issue with visualisation not appearing

--- app/plot.py
+++ app/plot.py
@@ -1,5 +1,7 @@
 from graph import Graph
 from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 """Class that will generate a simple plot chart"""
 class Plot(Graph):
@@ -12,9 +14,9 @@ class Plot(Graph):
 		graph.line(x_axis, y_axis, legend="Temp.", line_width=2)
 		show(graph)
 
-		return self.generate_file("Movies")
+		return self.generate_file(graph, CDN, "Movies")
 
-	def generate_file(self, title):
-		return output_file(title + ".html")
+	def generate_file(self, plot, cdn_import, title):
+		return file_html(plot, cdn_import, title)
 
 
