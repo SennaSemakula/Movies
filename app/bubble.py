a6a737cb9c2faf90f84b17d1683745bae1e127fe
sennasemakula <sennasemakula@hotmail.co.uk>
2018-10-07 16:10:44 +0100

    Visualiastions generated are now saved inside /templates

--- app/bubble.py
+++ app/bubble.py
@@ -4,27 +4,23 @@ import plotly
 
 """Class that will generate a simple plot chart"""
 class Bubble(Graph):
-	def __init__(self, data, title):
-		self.data = data
-		self.title = title
+    def __init__(self, data, title):
+        self.data = data
+        self.title = title
 
-	def draw_graph(self, x_axis, y_axis, circ_radius):
-		chart = go.Scatter(
-	    x=x_axis,
-	    y=y_axis,
-	    mode='markers',
-	    marker=dict(size=circ_radius,)
-		)
+    def draw_graph(self, x_axis, y_axis, circ_radius):
+        chart = go.Scatter(x = x_axis,y = y_axis,
+                mode='markers',marker=dict(size=circ_radius,))
 
-		data = [chart]
+        data = [chart]
 
-		return data
+        return data
 
-	def generate_file(self, chart_data, filename, open_chart=False):
-		graph_html = plotly.offline.plot({
-							"data": chart_data,
-							"layout": go.Layout(title=self.title)}, 
-		                    filename=filename,
-		                    auto_open=open_chart)
+    def generate_file(self, chart_data, filename, open_chart=False):
+        graph_html = plotly.offline.plot({
+                            "data": chart_data,
+                            "layout": go.Layout(title=self.title)},
+                            filename="templates/{}".format(filename),
+                            auto_open=open_chart)
 
-		return graph_html
+        return graph_html
